2454566d0c2a24f62bda71f4a4aaad6934a0cbcc
shoaib zubair <sz712316@gmail.com>
2025-05-07 16:39:04 +0530

    fixed Focus Timer drop down issue

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(bodyParser.json());
